726c751f3e3105b536639b562c59123cb723f451
phamdatqn <phamdat.qn@gmail.com>
2023-01-13 11:20:29 +0700

    update day 2

--- data-base/product-sprint3.sql
+++ data-base/product-sprint3.sql
@@ -1,88 +1,96 @@
-create database if not exists product_sprint3;
+create
+database if not exists product_sprint3;
 
-use product_sprint3;
+use
+product_sprint3;
 
-create table if not exists user(
-	username varchar(30) primary key,
-    password varchar(200),
-    is_delete bit default 0
+CREATE TABLE IF NOT EXISTS user (
+    username VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(200),
+    is_delete BIT DEFAULT 0
 );
 
-create table  if not exists role(
-	id int primary key auto_increment,
-    name varchar(30),
-    is_delete bit default 0
+CREATE TABLE IF NOT EXISTS role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30),
+    is_delete BIT DEFAULT 0
 );
 
-create table if not exists user_role(
-	username varchar(50),
-    role_id int,
-    is_delete bit default 0,
-    foreign key(username) references user(username),
-    foreign key(role_id) references role(id),
-    primary key(username, role_id)
+CREATE TABLE IF NOT EXISTS user_role (
+    username VARCHAR(50),
+    role_id INT,
+    is_delete BIT DEFAULT 0,
+    FOREIGN KEY (username)
+        REFERENCES user (username),
+    FOREIGN KEY (role_id)
+        REFERENCES role (id),
+    PRIMARY KEY (username , role_id)
 );
 
-create table if not exists customer(
-	id int primary key auto_increment,
-	name varchar(30),
-	day_of_birth varchar(30),
-	gender int,
-	id_card varchar(12),
-	email varchar(100),
-	address varchar(200),
-	phone_number varchar(15),
-	username varchar(30) unique,
-    is_delete bit default 0,
-	foreign key (username) references user(username)
+CREATE TABLE IF NOT EXISTS customer (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30),
+    day_of_birth VARCHAR(30),
+    gender INT,
+    id_card VARCHAR(12),
+    email VARCHAR(100),
+    address VARCHAR(200),
+    phone_number VARCHAR(15),
+    username VARCHAR(30) UNIQUE,
+    is_delete BIT DEFAULT 0,
+    FOREIGN KEY (username)
+        REFERENCES user (username)
 );
 
-create table if not exists product_type(
-	id int primary key auto_increment,
-	name varchar(30),
-	is_delete bit default 0
-    );
+CREATE TABLE IF NOT EXISTS product_type (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30),
+    is_delete BIT DEFAULT 0
+);
 
-create table if not exists size(
-	id int primary key auto_increment,
-	name varchar(30),
-	is_delete bit default 0
-    );
-    
-create table if not exists product(
-	id int primary key auto_increment,
-	name varchar(100),
-    price int,
-    discount int,
-    manufacturer varchar(50),
-    `describe` varchar(255),
-    image text,
+CREATE TABLE IF NOT EXISTS size (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30),
+    is_delete BIT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS product_detail(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    price INT,
+    discount INT,
+    manufacturer VARCHAR(50),
+    `describe` VARCHAR(255),
+    image TEXT,
     product_type_id int,
-	is_delete bit default 0,
-    foreign key (product_type_id) references product_type(id)
-    );
+    is_delete BIT DEFAULT 0,
+    foreign key(product_type_id) references product_type(id)
     
-create table if not exists product_size(
-	id int primary key auto_increment,
-	size_id int,
-	product_id int,
-    quantity int,
-	is_delete bit default 0,
-	foreign key (size_id) references size(id),
-	foreign key (product_id) references product(id)
-    );
-    
-create table if not exists order_detail(
-	id int primary key auto_increment,
-    quantity int,
-    day_payment datetime,
-    customer_id int,
-    product_size_id int,
-    is_delete bit default 0,
-    is_pay bit default 0,
-	foreign key (customer_id) references customer(id),
-    foreign key (product_size_id) references product_size(id)
-    );
+);
+
+CREATE TABLE IF NOT EXISTS product(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    size_id INT,
+    product_id INT,
+    quantity INT,
+        product_detail_id int,
+    is_delete BIT DEFAULT 0,
+    FOREIGN KEY (size_id)REFERENCES size (id),
+    	foreign key (product_detail_id) references product_detail(id)
+);
+
+CREATE TABLE IF NOT EXISTS order_detail (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    quantity INT,
+    day_payment DATETIME,
+    customer_id INT,
+    product_id INT,
+    is_delete BIT DEFAULT 0,
+    is_pay BIT DEFAULT 0,
+    FOREIGN KEY (customer_id)REFERENCES customer (id),
+    FOREIGN KEY (product_id)REFERENCES product (id)
+
+);
 
     
     
